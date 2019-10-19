857747ae99495fd8aa9f46f5c89494e82d6a2146
Anvesh <anveshmandadi821@gmail.com>
2019-10-19 15:40:10 -0500

    Mobile_ICP1

---
+++ Mobile_Module/Mobile_ICP1/AndroidApp/app/src/main/java/tutorial/cs5551/com/translateapp/HomeActivity.java
@@ -0,0 +1,23 @@
+package tutorial.cs5551.com.translateapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void logOut(View v){
+        Intent redirect = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(redirect);
+    }
+}
